80d3e0bd56fbf0ed68a11ef3bdc4114b481a3179
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2022-02-07 10:39:36 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,13 @@
-sudo apt update && sudo apt upgrade -y && sudo apt install firefox -y && sudo apt install wireshark --yes && sudo usermod -aG wireshark $(whoami)
-firefox
-wireshark
+FILE=/workspace/Gitpod-VNC/aliases.sh
+FILE1=/workspace/Gitpod-VNC/startup.sh
+if [ -f "$FILE" ]; then 
+    clear && echo "aliases file exists."
+else
+    touch aliases.sh
+    chmod +x aliases.sh
+    curl https://gitlab.com/nstoc6696/Gitpod-VNC/-/raw/master/aliases.sh > aliases.sh
+    rm README.md
+fi
+if [ -f "$FILE1" ]; then
+    rm $FILE1
+fi
